3e478c4e00415afffb816b6cd75d6c2a5420cb09
Piotrek Rudnicki <p.rudnicki@developerodzera.pl>
2018-11-07 22:19:24 +0100

    Upload whole project on GitHub

---
+++ Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleControl : MonoBehaviour {
+
+	public float timeFactor = 10f;
+	private Vector3 playerPos = new Vector3 (0,-10,0);
+
+	void Start()
+	{
+		Cursor.visible = false;
+	}
+	void Update () {
+
+		if (Input.GetKey(KeyCode.Escape))
+			{
+				Cursor.visible = true;
+			}
+		float xPosition = transform.position.x + (Input.GetAxis("Mouse X"))*Time.deltaTime * timeFactor;
+		playerPos = new Vector3 (Mathf.Clamp (xPosition, -8f, 8f), -10, 0);
+		transform.position = playerPos;
+	}
+}
